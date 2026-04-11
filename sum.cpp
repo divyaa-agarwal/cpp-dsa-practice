34dbffe9072339e179d72669def6d7711f827716
divyaa-agarwal <divyanshilko2411@gmail.com>
2026-04-11 20:05:23 +0530

    SUM OF NUMBERS

---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() { 
+    int a,b,c,d;
+    cout << "ENTER THE NUMBERS" << endl;
+    cin >> a >> b >> c >> d;
+    cout<<"sum="<<(a+b+c+d)<<endl;
+return 0;
+}
