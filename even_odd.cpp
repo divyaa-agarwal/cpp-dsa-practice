1a963485d868aff6d2806ec66f3de296a9c29b08
divyaa-agarwal <divyanshilko2411@gmail.com>
2026-04-12 18:17:43 +0530

    EVEN ODD

---
+++ even_odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"ENTER TO NUMBER:"<<endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"EVEN NUMBER"<<endl;
+    }else{
+        cout<<"ODD NUMBER"<<endl;
+    }
+}
